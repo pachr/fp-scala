d7c4d16ecffddf93620f089488e2b2875612e2d4
Pac <paulalexandre@mymushin.com>
2017-02-02 17:20:16 +0100

    feat(funsets): basic functions, queries and transformations on funsets
    All tests are ok
    
    No breaking changes

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -22,34 +22,34 @@ object FunSets {
    * Returns the set of the one given element.
    */
   def singleElementSet(elem: Int): FunSet =
-    ??? //TODO : singleton set
+    (x:Int) => x == elem
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
   def union(s: FunSet, t: FunSet): FunSet =
-    ??? //TODO : union
+    (x:Int) => contains(s,x) || contains(t,x)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` or `t`.
    */
   def intersect(s: FunSet, t: FunSet): FunSet =
-    ??? // TODO : interesection between 2 sets
+    (x:Int) => s(x) && t(x)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
   def diff(s: FunSet, t: FunSet): FunSet =
-    ??? // TODO : difference between 2 sets
+    (x:Int) => s(x) && !t(x)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
   def filter(s: FunSet, p: Int => Boolean): FunSet =
-    ??? //TODO : return a new set in which only elements verifying the predicate are kept
+    (x:Int) => s(x) && p(x)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -61,26 +61,31 @@ object FunSets {
    */
   def forall(s: FunSet, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a < -bound) true
+      else if (contains(s,a) && !p(a)) false
+      else iter(a-1)
     }
-    iter(???)
+    iter(bound)
   }
-  //TODO: forall : check if all integers verify the predicate
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: FunSet, p: Int => Boolean): Boolean =
-    ??? //TODO: check if at least an integer verifies the predicate
+  def exists(s: FunSet, p: Int => Boolean): Boolean = {
+    def iter(a: Int): Boolean = {
+      if (a < -bound) false
+      else if (contains(s,a) && p(a)) true
+      else iter(a-1)
+    }
+    iter(bound)
+}
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
   def map(s: FunSet, f: Int => Int): FunSet =
-    ??? //TODO : transform into a new set
+    (x:Int) =>{  exists(s, (y:Int) => { x == f(y) } ) }
 
   def toSet(ints: List[Int]): FunSet =
     ??? //TODO : (optional) convert a list to Set
